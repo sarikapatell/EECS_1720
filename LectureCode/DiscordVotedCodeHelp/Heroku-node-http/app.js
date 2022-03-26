68b8e1be176b973f1b5991c5dcaf103b4e8179e4
sarikapatell <98135072+sarikapatell@users.noreply.github.com>
2022-03-25 23:21:51 -0400

    Add files via upload

---
+++ LectureCode/DiscordVotedCodeHelp/Heroku-node-http/app.js
@@ -0,0 +1,44 @@
+// Setting up a node.js website is learned from https://www.youtube.com/watch?v=VShtPwEkDD0
+
+const http = require('http');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', function(error, data){
+        if(error) {
+            res.writeHead(404)
+            res.write(' Error: File Not Found');
+        }else{
+            res.write(data);
+        }
+        res.end();
+    });
+    // fs.readFile('script.js', function(error, data){
+    //     if(error) {
+    //         res.writeHead(404)
+    //         res.write(' Error: File Not Found')
+    //     }else{
+    //         res.write(data)
+    //     }
+    //     res.end()
+    // });
+    // fs.readFile('style.css', function(error, data){
+    //     if(error) {
+    //         res.writeHead(404)
+    //         res.write(' Error: File Not Found')
+    //     }else{
+    //         res.write(data)
+    //     }
+    //     res.end()
+    // });
+});
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Something went wrong', error);
+    }else {
+        console.log('Server is working on port' + port);
+    }
+});
